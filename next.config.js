44d2dfd4c7bbbbabdfb2a68b93f48929b29e60b6
Oscar Corcho <ojc8818@gmail.com>
2022-08-16 11:03:18 -0500

    adding tests, tailwind, translations

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = {
+  nextConfig
+}
+
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+  },
+}
